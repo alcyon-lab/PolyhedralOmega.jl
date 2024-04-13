359ac2be3743a5d1b08ad7cccbb1509399161a5f
İsmail Tapan <ismltpn@gmail.com>
2024-04-13 15:50:07 +0300

    import code from the old project

---
+++ src/EliminateCoordinates.jl
@@ -0,0 +1,63 @@
+using Cones
+
+function eliminate_last_coordinate(cone::Cone{T})::CombinationOfCones{T} where {T<:Value}
+    result = CombinationOfCones{T}()
+    last_apex = cone.apex[end]
+    indices = []
+    if last_apex < 0
+        for j in eachindex(cone.rays)
+            if cone.rays[j].direction[end] > 0
+                push!(indices, j)
+            end
+        end
+    else
+        for j in eachindex(cone.rays)
+            if cone.rays[j].direction[end] < 0
+                push!(indices, j)
+            end
+        end
+        rays = collect(map(r -> Ray{T}(r.direction[1:end-1], r.apex[1:end-1]), cone.rays))
+        result += Cone(rays, cone.apex[1:end-1], cone.openness[:], cone.sign)
+    end
+    for j in indices
+        ray_j = cone.rays[j]
+        rays = Vector{Ray{T}}([])
+        for (i, ray_i) in enumerate(cone.rays)
+            sign = last_apex >= 0 ? 1 : -1
+            if i == j
+                ray = -sign * ray_i
+            else
+                ray = sign * (ray_j * ray_i.direction[end] - ray_i * ray_j.direction[end])
+            end
+            reduced_ray = primitive(Ray{T}(ray.direction[1:end-1], ray.apex[1:end-1]))
+            push!(rays, reduced_ray)
+        end
+
+        apex = Vector{T}(cone.apex - (last_apex // ray_j.direction[end]) .* ray_j.direction)
+        reduced_apex = apex[1:end-1]
+
+        openness = deepcopy(cone.openness)
+        openness[j] = false
+
+        new_cone = Cone(rays, reduced_apex, openness, cone.sign)
+        new_cone = flip(new_cone)
+        result += new_cone
+    end
+    return result
+end
+
+function eliminate_coordinates(cone::Cone{T}, k::Int)::CombinationOfCones{T} where {T<:Value}
+    combination = CombinationOfCones{T}()
+    combination += cone
+    for i = 1:k
+        innercombination = CombinationOfCones{T}()
+        for (cone, count) in combination.cones
+            res = eliminate_last_coordinate(cone)
+            for j in 1:count
+                innercombination += res
+            end
+        end
+        combination = innercombination
+    end
+    return combination
+end
--- src/PolyhedralOmega.jl
+++ src/PolyhedralOmega.jl
@@ -1,5 +1,86 @@
 module PolyhedralOmega
 
-# Write your package code here.
+using Polynomials
+using Cones
+using Values
+
+include("EliminateCoordinates.jl")
+include("MacmahonLifting.jl")
+
+export run_polyhedral_omega
+
+function run_polyhedral_omega(A::Matrix{T}, b::Vector{T}; rf_as_string::Bool=false) where {T<:Value}
+    macmahon_cone = macmahon_lifting(A, b)
+    list_of_cones = eliminate_coordinates(macmahon_cone, size(b, 1))
+    fpps = Dict()
+    r_str = ""
+    r = CombinationOfRationalFunctions{Integer}()
+    for (cone, count) in list_of_cones.cones
+        cone = Cone{Number}(cone.rays, cone.apex, cone.openness, cone.sign)
+        fpp = enumerate_fundamental_parallelepiped(cone)
+        fpps[cone] = fpp
+        if rf_as_string
+            cone_rf_str = compute_rational_function_str(cone, fpp)
+            if count != 1
+                cone_rf_str = "($(count)*($(cone_rf_str)))"
+            end
+            if (length(r_str) > 0)
+                r_str = r_str * " + " * cone_rf_str
+            else
+                r_str = cone_rf_str
+            end
+        else
+            cone_rf_s = compute_rational_function(cone, fpp) * count
+            r = cone_rf_s + r
+        end
+    end
+    if rf_as_string
+        return list_of_cones, fpps, r_str
+    else
+        return list_of_cones, fpps, r
+    end
+
+end
+
+function run_polyhedral_omega(A::Matrix{T}, b::Vector{T}, f::Vector{T}, values::Vector{<:Number}; rf_as_string::Bool=false) where {T<:Value}
+    α = Symbol("α")
+    macmahon_cone = macmahon_lifting(A, b, f, symbol=α)
+    list_of_cones = eliminate_coordinates(macmahon_cone, size(b, 1))
+    num_of_variables = length(macmahon_cone.apex)
+    x = [Symbol("x_$(i)") for i in 1:num_of_variables]
+    ret = []
+    fpps = []
+    for val in values
+        r = 0
+        r_str = ""
+        inner_fpps = Dict()
+        for (cone, count) in list_of_cones.cones
+            s_cone = substitute_cone(cone, Dict(α => val))
+            fpp = enumerate_fundamental_parallelepiped(s_cone)
+            inner_fpps[s_cone] = fpp
+            if rf_as_string
+                cone_rf_str = compute_rational_function_str(s_cone, fpp)
+                if count != 1
+                    cone_rf_str = "($(count)*($(cone_rf_str)))"
+                end
+                if (length(r_str) > 0)
+                    r_str = r_str * " + " * cone_rf_str
+                else
+                    r_str = cone_rf_str
+                end
+            else
+                cone_rf_s = compute_rational_function(s_cone, fpp) * count
+                r += cone_rf_s
+            end
+        end
+        push!(fpps, inner_fpps)
+        if rf_as_string
+            push!(ret, r_str)
+        else
+            push!(ret, r)
+        end
+    end
+    return list_of_cones, fpps, ret
+end
 
 end
